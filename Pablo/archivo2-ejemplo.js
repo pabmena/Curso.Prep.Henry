578ab5f7a27bd3ae3917bcf864b4c75f8e25f6ef
pabmena <you@example.com>
2021-07-02 15:09:09 -0300

    finish homeworks 1y2

---
+++ Pablo/archivo2-ejemplo.js
@@ -0,0 +1,31 @@
+Definición de variable: 
+Es un término que permite almacenar un valor (numérico o no, pueden ser palabras) y poder usarlo después.
+
+Definición de strings:
+Las "strings" son bloques de texto, siempre se definirán entre comillas, ya sea simple o doble. Cualquier texto entre comillas es una cadena o string.
+
+Definición de funciones (argumentos, return):
+Las funciones son una parte muy importante de todo lenguaje de programación y sobre todo en JavaScript. Son tipos particulares de Objetos, llamados callable objects u objetos invocables, por lo que tienen las mismas propiedades que cualquier objeto.
+Una función comenzará con la palabra clave function, esto le dice a lo que sea que esté ejecutando tu programa que lo que sigue es una función y que debe tratarse como tal. Después de eso viene el nombre de la función, nos gusta dar nombres de funciones que describan lo que hacen. Luego viene un paréntesis abierto y uno cercano. Y finalmente, abra y cierre los corchetes. Entre estos corchetes es donde irá todo nuestro código a ejecutar.
+
+Ahora que podemos ejecutar una función básica, vamos a comenzar a pasarle argumentos:
+Si agregamos una variable a los paréntesis cuando declaramos la función, podemos usar esta variable dentro de nuestra función. Iniciamos el valor de esta variable pasándola a la función cuando la llamamos. Entonces en este caso nombre = 'Martin'. Este es nuestro argumento. Podemos agregar múltiples argumentos colocando una coma entre ellos:
+Declaración "return": no vamos a usar console.log con todo lo que salga de una función. Lo más probable es que queramos devolver algo. En este caso es la suma de los dos números. Piense en la declaración de retorno ("return") como la única forma en que los datos escapan de una función. No se puede acceder a nada más que a lo que se devuelve fuera de la función. También tenga en cuenta que cuando una función golpea una declaración de retorno, la función detiene inmediatamente lo que está haciendo y "devuelve" lo especificado.
+
+Definición de Boolean:
+Los booleanos provienen de la lógica de Boole. Es un concepto que alimenta el código binario y el núcleo de las computadoras. Es posible que haya visto código binario en el pasado (0001 0110…), esto es lógica booleana. Esencialmente significa que tiene dos opciones, activar o desactivar, 0 o 1, verdadero o falso. En Javascript usamos booleanos para significar verdadero o falso. Esto puede parecer simple al principio, pero puede complicarse más adelante. Los valores posibles de un dato booleando en JS son: true o false.
+
+Definicion for:
+La instrucción for crea un bucle que consta de tres expresiones opcionales, encerradas entre paréntesis y separadas por punto y coma, seguidas de una instrucción (generalmente una instrucción de bloque ) que se ejecutará en el bucle.
+
+Definición de &&, ||, ! :
+También podemos combinar dos expresiones de igualdad y preguntar si alguna de las dos es verdadera, si ambas son verdaderas o si ninguna de ellas es verdadera. Para hacer esto, utilizaremos operadores lógicos.
+&&
+El primer operador lógico que veremos es el operador "Y" ("AND"). Está escrito con dos símbolos (&&). Esto evaluará ambas expresiones y devolverá verdadero si AMBAS expresiones son true. Si uno (o ambos) de ellos es falso, este operador devolverá false.
+El siguiente es el operador "Ó" ("OR"). Está escrito con dos barras verticales (||). Determinará si una de las expresiones es true. Devolverá true si una (o ambas) de las expresiones es true. Devolverá false si AMBAS expresiones son false.
+El último operador lógico es el operador "NOT" ("NO"). Está escrito como un solo signo de exclamación (!). Vimos este operador antes al determinar la igualdad (!==). Como antes, el operador NOT devolverá el valor booleano opuesto de lo que se le pasa.
+
+
+03-JS-II
+1. Math.max(x, y);  puede ser algo asi usando esta funcion ???
+
